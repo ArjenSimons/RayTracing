fe7928b5e570d0972ff62612e4b0db82ac86a437
robertoost <github@robertoo.st>
2022-01-25 01:26:56 +0100

    feat(PostProcessing): pass renderbuffer & uv

--- src/PostProcessing/PostProcessing.cpp
+++ src/PostProcessing/PostProcessing.cpp
@@ -1,7 +1,7 @@
 #include "precomp.h"
 
 //TODO: Compute post processing effects on GPU
-void PostProcessing::Vignette(float outerRadius, float smoothness, float intensity)
+void PostProcessing::Vignette(Color renderBuffer[SCRWIDTH][SCRHEIGHT], float2 uv[SCRWIDTH][SCRHEIGHT], float outerRadius, float smoothness, float intensity)
 {
 	outerRadius = clamp(outerRadius, 0.0f, 1.0f);
 	intensity = clamp(intensity, 0.0f, 1.0f);
@@ -19,7 +19,7 @@ void PostProcessing::Vignette(float outerRadius, float smoothness, float intensi
 	}
 }
 
-void PostProcessing::GammaCorrection(float gamma)
+void PostProcessing::GammaCorrection(Color renderBuffer[SCRWIDTH][SCRHEIGHT], float gamma)
 {
 	float invGamma = 1 / gamma;
 
@@ -33,7 +33,9 @@ void PostProcessing::GammaCorrection(float gamma)
 	}
 }
 
-void PostProcessing::ChromaticAberration(int2 redOffset, int2 greenOffset, int2 blueOffset)
+Color tempBuffer[SCRWIDTH][SCRHEIGHT];
+
+void PostProcessing::ChromaticAberration(Color renderBuffer[SCRWIDTH][SCRHEIGHT], int2 redOffset, int2 greenOffset, int2 blueOffset)
 {
 	for (int i = 0; i < SCRWIDTH; i++) for (int j = 0; j < SCRHEIGHT; j++)
 	{
--- src/PostProcessing/PostProcessing.h
+++ src/PostProcessing/PostProcessing.h
@@ -2,8 +2,8 @@
 static class PostProcessing
 {
 public:
-	static void Vignette(float outerRadius, float smoothness, float intensity);
-	static void GammaCorrection(float gamma);
-	static void ChromaticAberration(int2 redOffset, int2 greenOffset, int2 blueOffset);
+	static void Vignette(Color renderBuffer[SCRWIDTH][SCRHEIGHT], float2 uv[SCRWIDTH][SCRHEIGHT], float outerRadius, float smoothness, float intensity);
+	static void GammaCorrection(Color renderBuffer[SCRWIDTH][SCRHEIGHT], float gamma);
+	static void ChromaticAberration(Color renderBuffer[SCRWIDTH][SCRHEIGHT], int2 redOffset, int2 greenOffset, int2 blueOffset);
 };
 
