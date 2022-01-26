6554b770f3e7e0c40da40d9d5b47483dea0cc561
robertoost <github@robertoo.st>
2022-01-26 22:56:31 +0100

    feat(Scene): move to header, add animation

--- src/Scenes/Scene.h
+++ src/Scenes/Scene.h
@@ -3,18 +3,45 @@
 class Scene
 {
 private: 
-	std::vector<Intersectable*> objects;
-	std::vector<LightSource*> lightSources;
+	vector<Intersectable*> objects;
+	vector<LightSource*> lightSources;
 	TopLevelBVH* topBVH;
+	function<void(float)> animations;
 public:
 
-	Scene();
-	~Scene();
+	Scene(vector<Intersectable*> objects, vector<LightSource*> lightSources, TopLevelBVH* topBVH) {
+		this->objects = objects;
+		this->lightSources = lightSources;
+		this->topBVH = topBVH;
+	};
+	Scene() {}
+	~Scene() {}
 
-	void AddBVH(TopLevelBVH* bvh);
-	void AddObject(Intersectable* object);
-	void AddLightSource(LightSource* lightSource);
-	std::vector<Intersectable*> GetObjects() const { return objects; }
-	std::vector<LightSource*> GetLights() const { return lightSources; };
-	TopLevelBVH* GetBVH() const { return topBVH; }
+	inline void AddBVH(TopLevelBVH* bvhTree) {
+		topBVH = bvhTree; 
+	}
+
+	inline void AddObject(Intersectable* object) {
+		objects.push_back(object); 
+	}
+
+	inline void AddLightSource(LightSource* lightSource) { 
+		lightSources.push_back(lightSource); 
+	}
+
+	inline void AddAnimations(function<void(float)> animations) { 
+		this->animations = animations; 
+	}
+
+	inline void Animate(float deltaTime) {
+		int i = 0;
+		// Check if the std::function exists using its implicit bool conversion.
+		if (animations) {
+			animations(deltaTime); 
+		}
+	}
+
+	inline vector<Intersectable*> GetObjects() const { return objects; }
+	inline vector<LightSource*> GetLights() const { return lightSources; };
+	inline TopLevelBVH* GetBVH() const { return topBVH; }
 };
